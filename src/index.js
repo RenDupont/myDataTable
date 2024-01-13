7ad9395e5936ce1f276422e2907ae81126f92add
Dupont <zeshin59@gmail.com>
2024-01-13 10:47:00 +0100

    simpleDatatable without css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import MyComponent from './lib/MyComponent';
+
+const columns = [
+    { key: 'firstName', label: 'First Name' },
+    { key: 'lastName', label: 'Last Name' },
+    { key: 'StartDate', label: 'startDate' },
+    { key: 'Department', label: 'department' },
+    { key: 'DateofBirth', label: 'dateOfBirth' },
+    { key: 'Street', label: 'street' },
+    { key: 'City', label: 'city' },
+    { key: 'State', label: 'state' },
+    { key: 'ZipCode', label: 'zipCode' },
+];
+  
+const data = [
+    {
+        firstName: 'test',
+        lastName: 'test',
+        StartDate: 'today',
+        Department: 'Sales',
+        DateofBirth: 'yesterday',
+        Street: '1234',
+        City: 'here',
+        State: 'CA',
+        ZipCode: '1234',
+    },
+    {
+        firstName: 'test2',
+        lastName: 'test2',
+        StartDate: 'today',
+        Department: 'Sales',
+        DateofBirth: 'yesterday',
+        Street: '1234',
+        City: 'here',
+        State: 'CA',
+        ZipCode: '1234',
+    },
+    {
+        firstName: 'test3',
+        lastName: 'test3',
+        StartDate: 'today',
+        Department: 'Sales',
+        DateofBirth: 'yesterday',
+        Street: '1234',
+        City: 'here',
+        State: 'CA',
+        ZipCode: '1234',
+    },
+    {
+        firstName: 'test4',
+        lastName: 'test4',
+        StartDate: 'today',
+        Department: 'Sales',
+        DateofBirth: 'yesterday',
+        Street: '1234',
+        City: 'here',
+        State: 'CA',
+        ZipCode: '1234',
+    },
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <MyComponent data={data} columns={columns} />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
